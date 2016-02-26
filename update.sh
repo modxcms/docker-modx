4a07a9cdf5e7e8b6c0fc27ff449b0b68c388f616
Vadim Homchik <homchik@gmail.com>
2016-02-26 13:46:06 +0300

    update.sh

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+current="$(curl -sSL 'https://api.github.com/repos/modxcms/revolution/tags' | sed -n 's/^.*"name": "v\([^"]*-pl\)".*$/\1/p' | head -n1)"
+
+curl -o modx.zip -sSL https://modx.com/download/direct/modx-$current.zip
+
+sha1="$(sha1sum modx.zip | sed -n 's/ .*//p')"
+
+for variant in apache fpm; do
+	(
+		set -x
+
+		sed -ri '
+			s/^(ENV MODX_VERSION) .*/\1 '"$current"'/;
+			s/^(ENV MODX_SHA1) .*/\1 '"$sha1"'/;
+		' "$variant/Dockerfile"
+
+		cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
+	)
+done
+
+rm modx.zip
